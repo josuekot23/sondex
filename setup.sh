76a3e6d1ebc16a4a417f00d86acb44c82819d8df
jkotokpo23 <kotokpoj@gmail.com>
2026-07-27 14:36:23 +0100

    crontab V1

--- setup.sh
+++ setup.sh
@@ -21,5 +21,56 @@ echo "Installation des dépendances dans l'environnement virtuel..."
 pip install -r requirements.txt
 
 echo "=== Installation terminée ! ==="
-echo "Pour activer l'environnement virtuel : source venv/bin/activate"
-echo "Pour exécuter votre script : python votre_script.py"
+
+
+# Remplissage de la crontab 
+echo "Configuration du lancement automatique au démarrage..."
+
+# Chemin absolu du dossier sondex
+DOSSIER="$(cd "$(dirname "$0")" && pwd)"
+# Chemin vers le Python de l'environnement virtuel
+PYTHON_VENV="$DOSSIER/venv/bin/python"
+
+# MESURE 
+
+# Chemin vers le script Python : mesures
+SCRIPT="$DOSSIER/mesures.py"
+# Ajout de la tâche cron au démarrage (si elle n'existe pas déjà)
+CRON_JOB="@reboot $PYTHON_VENV $SCRIPT >> $DOSSIER/mesures.log 2>&1"
+(crontab -l 2>/dev/null | grep -v "$SCRIPT"; echo "$CRON_JOB") | crontab -
+echo "Crontab configurée : mesures.py sera lancé au démarrage."
+
+# Chemin vers le script Python : mesures
+SCRIPT="$DOSSIER/temperature.py"
+# Ajout de la tâche cron au démarrage (si elle n'existe pas déjà)
+CRON_JOB="@reboot $PYTHON_VENV $SCRIPT >> $DOSSIER/temperature.log 2>&1"
+(crontab -l 2>/dev/null | grep -v "$SCRIPT"; echo "$CRON_JOB") | crontab -
+echo "Crontab configurée : temperature.py sera lancé au démarrage."
+
+
+# Remplissage de la crontab : check mesures
+SCRIPT_SH="$DOSSIER/check_mesures.sh"
+# Rendre le script exécutable
+chmod +x "$SCRIPT_SH"
+# Ajout dans la crontab au démarrage
+CRON_JOB="@reboot sleep 30 && $SCRIPT_SH >> $DOSSIER/check_mesures.log 2>&1"
+(crontab -l 2>/dev/null | grep -v "$SCRIPT_SH"; echo "$CRON_JOB") | crontab -
+echo "Crontab configurée : check_mesures.sh sera lancé au démarrage."
+
+# Remplissage de la crontab : check mosquitto
+SCRIPT_SH="$DOSSIER/check_mosquitto.sh"
+# Rendre le script exécutable
+chmod +x "$SCRIPT_SH"
+# Ajout dans la crontab au démarrage
+CRON_JOB="@reboot sleep 30 && $SCRIPT_SH >> $DOSSIER/check_mosquitto.log 2>&1"
+(crontab -l 2>/dev/null | grep -v "$SCRIPT_SH"; echo "$CRON_JOB") | crontab -
+echo "Crontab configurée : check_mosquitto.sh sera lancé au démarrage."
+
+# Remplissage de la crontab : check mesures
+SCRIPT_SH="$DOSSIER/check_temperature.sh"
+# Rendre le script exécutable
+chmod +x "$SCRIPT_SH"
+# Ajout dans la crontab au démarrage
+CRON_JOB="@reboot sleep 30 && $SCRIPT_SH >> $DOSSIER/check_temperature.log 2>&1"
+(crontab -l 2>/dev/null | grep -v "$SCRIPT_SH"; echo "$CRON_JOB") | crontab -
+echo "Crontab configurée : check_temperature.sh sera lancé au démarrage."
